b8af8fc51b9298bd474a13623c859e1e35780a9e
Aditya <75035219+pegvin@users.noreply.github.com>
2025-06-22 10:10:41 +0530

    Update Build Script & Setup CI

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ LD=${LD:-gcc}
 BUILD="build"
 SOURCES="src/main.c src/imgui.c src/os/os.c src/base/math.c src/base/arena.c src/base/string.c"
 OBJECTS=$(echo "$SOURCES" | sed "s|\([^ ]*\)\.c|$BUILD/\1.c.o|g")
-CFLAGS="-std=c99 -Isrc/ -march=native -MMD -MP -Wall -Wextra -Wpadded -pedantic -ffast-math -D_DEFAULT_SOURCE=1"
-LFLAGS="-lX11"
+CFLAGS="-std=c99 -Isrc/ -march=native -MMD -MP -Wall -Wextra -Wpadded -pedantic -D_DEFAULT_SOURCE=1 -ffast-math -fvisibility=hidden -fdata-sections -ffunction-sections"
+LFLAGS="-fvisibility=hidden -Wl,--gc-sections -lX11"
 CMD=${1:-}
 
 mkdir -p $BUILD "$BUILD/.ccache"
@@ -20,7 +20,7 @@ elif [ "$CMD" = "bear" ]; then
 	bear --append --output "$BUILD/compile_commands.json" -- $0 # github.com/rizsotto/Bear
 	exit 0
 elif [ "$CMD" = "release" ]; then
-	CFLAGS="$CFLAGS -O3 -ffast-math"
+	CFLAGS="$CFLAGS -O3"
 elif [ "$CMD" = "" ]; then
 	CFLAGS="$CFLAGS -O0 -g -fsanitize=address,undefined"
 	LFLAGS="$LFLAGS -fsanitize=address,undefined"
@@ -31,10 +31,10 @@ fi
 
 echo "$SOURCES 0" | tr ' ' '\n' | while read source; do
 	if [ "$source" = "0" ]; then wait; exit 0; fi
-	echo "Compiling $source"
+	echo "CC $source"
 	mkdir -p "$(dirname "$BUILD/$source.o")"
 	CCACHE_DIR="$BUILD/.ccache" ccache $CC $CFLAGS -c $source -o "$BUILD/$source.o" &
 done
 
 $LD $OBJECTS $LFLAGS -o "$BUILD/imgui"
-echo "  Linking $BUILD/imgui"
+echo "LD $BUILD/imgui"
